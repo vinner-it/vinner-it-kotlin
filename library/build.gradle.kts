5e89184b6012c1e40bb644044a0b09c722ed7181
Shanmukha <shanmukha.vaddi@scania.com>
2024-05-16 13:31:40 +0200

    using vanniktech publish plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     //alias(libs.plugins.androidLibrary)
     id("module.publication")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 kotlin {
@@ -43,3 +48,50 @@ android {
     }
 }
 */
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.vinner-it",
+        artifactId = "vinner-it-kotlin",
+        version = "1.0.0"
+    )
+    configure(
+        KotlinMultiplatform(
+            // configures the -javadoc artifact, possible values:
+            // - `JavadocJar.None()` don't publish this artifact
+            // - `JavadocJar.Empty()` publish an emprt jar
+            // - `JavadocJar.Javadoc()` to publish standard javadocs
+            // whether to publish a sources jar
+            JavadocJar.None(),
+            sourcesJar = true,
+        )
+    )
+    pom {
+        name.set("Vinner")
+        description.set("Open source VIN validation library.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/vinner-it/vinner-it-kotlin")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("dev.vinner@gmail.com")
+                name.set("Vinner")
+                url.set("https://github.com/vinner-it/")
+            }
+        }
+        scm {
+            url.set("https://github.com/vinner-it/vinner-it-kotlin/")
+            connection.set("scm:git:git://github.com/vinner-it/vinner-it-kotlin.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vinner-it/vinner-it-kotlin.git")
+        }
+    }
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+}
